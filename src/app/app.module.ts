437cec635f9e081ce6eb463355b473c2d9737819
Rafal Brzoska <rafal@brzoska.net.pl>
2022-12-05 13:14:16 +0100

    first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyTestComponent } from './my-test/my-test.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTestComponent
   ],
   imports: [
     BrowserModule,
