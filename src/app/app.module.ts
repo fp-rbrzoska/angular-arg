8750e41cb2da02dce341d52e26860155e670ad3d
Rafal Brzoska <rafal@brzoska.net.pl>
2022-12-05 13:23:58 +0100

    components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyTestComponent } from './my-test/my-test.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UnauthorizedAccessComponent } from './unauthorized-access/unauthorized-access.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyTestComponent
+    MyTestComponent,
+    HomeComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    UnauthorizedAccessComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/unauthorized-access/unauthorized-access.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fp-unauthorized-access',
+  templateUrl: './unauthorized-access.component.html',
+  styleUrls: ['./unauthorized-access.component.scss']
+})
+export class UnauthorizedAccessComponent {
+
+}
