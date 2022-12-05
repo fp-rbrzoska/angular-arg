9507608e341931aa137a6fc7ed33809e824f918d
Rafal Brzoska <rafal@brzoska.net.pl>
2022-12-05 14:00:46 +0100

    routing part 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 import { MyTestComponent } from './my-test/my-test.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { UnauthorizedAccessComponent } from './unauthorized-access/unauthorized-access.component';
 
 const routes: Routes = [
-  { path: '' , component:  MyTestComponent  },
-  { path: '**' , redirectTo: '/'  }
+  { path: '' , component:  HomeComponent  },
+  { path: 'contact' , component:  ContactComponent  },
+  { path: 'unathorized' , component:  UnauthorizedAccessComponent  },
+  { path: 'test' , component:  MyTestComponent  },
+  { path: '**' , component: PageNotFoundComponent}
 ];
 
 @NgModule({
