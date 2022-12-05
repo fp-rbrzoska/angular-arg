178b8d9affcdd46654dd72bd6155fc682ef5ec78
Rafal Brzoska <rafal@brzoska.net.pl>
2022-12-05 13:44:52 +0100

    routr part 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MyTestComponent } from './my-test/my-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '' , component:  MyTestComponent  },
+  { path: '**' , redirectTo: '/'  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
